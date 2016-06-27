66897f3a1dac35d06e4781661fa2e383a36cea80
Tatyana C <tatyana_c@Unikey.local>
2016-06-27 18:09:52 +0400

    finished app-page1,added responsive styles for header and pages app-data1 and searchresults.

--- src/media/js/src/app.js
+++ src/media/js/src/app.js
@@ -5,100 +5,113 @@
  */
 
 
+function onLinkClicked(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  var className = evt.target.className.replace(' selected', '');
+  $('.' + className).removeClass('selected');
+  evt.target.className += ' selected';
+  if ($('#' + evt.target.href.split('#')[1]).length > 0)
+    $('html, body').animate({
+        scrollTop: $('#' + evt.target.href.split('#')[1]).offset().top - $('.header-pane').outerHeight(true)
+    }, 200);
+}
 
+function onRepostBtnClick(event) {
+  event.stopPropagation();
+  $('.repost-popup').css('left', event.target.x - 150);
+  $('.repost-popup').css('top', event.target.y + 30);
+  $('.repost-popup').css('display', 'inline');
+  $(".repost-popup").css("position", "absolute");
+  $(document).one('click', function closeMenu(e) {
+    $('.repost-popup').css('display', 'none');
+  });
+  $(window).one('resize', function closeMenu(e) {
+    $('.repost-popup').css('display', 'none');
+    $(document).off('click');
+  });
+}
 
 
 function changeView(evt) {
-    var btns = document.getElementsByClassName('view-icon');
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].className = btns[i].className.replace(' selected', '');
-    }
-    var elem = evt.target;
-    while (elem.className.indexOf('view-icon') == -1) {
-        elem = elem.parentNode;
-    }
-    elem.className += ' selected';
+  var btns = document.getElementsByClassName('view-icon');
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].className = btns[i].className.replace(' selected', '');
+  }
+  var elem = evt.target;
+  while (elem.className.indexOf('view-icon') == -1) {
+    elem = elem.parentNode;
+  }
+  elem.className += ' selected';
 }
 
 // Note: Use 'search & replace' to rename 'App' to current project name an delete this note
-var App = new (function App() {
+var App = new(function App() {
 
-    this.dom = {
-        $window: $(window),
-        $document: $(document)
-    };
+  this.dom = {
+    $window: $(window),
+    $document: $(document)
+  };
 
-    this.env = {
-        mobileMode: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    };
+  this.env = {
+    mobileMode: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  };
 
-    this.modules = {};
-    this.utils = {};
+  this.modules = {};
+  this.utils = {};
 
-    // Base inits here:
-    CSSPlugin.defaultTransformPerspective = 300;
+  // Base inits here:
+  CSSPlugin.defaultTransformPerspective = 300;
 
-    // DOM-Ready inits:
-    var self = this;
-    $(function() {
-        self.dom.$body = $('body');
-        self.dom.$html = $('html');
+  // DOM-Ready inits:
+  var self = this;
+  $(function() {
+    self.dom.$body = $('body');
+    self.dom.$html = $('html');
 
-        // Modules init here
-        self.modules.SVGSprites.init();
+    // Modules init here
+    self.modules.SVGSprites.init();
 
-        $(window).on('resize', function closeMenu (e){
-            $('.title-pane').css('marginTop', $('.header-pane').outerHeight(true) );
-        });
-        $(window).resize();
-        $('.main-menu').on('click', function (event) {
-            event.stopPropagation();
-            $('.sidebar').addClass('show');
-            $(document).one('click', function closeMenu (e){
-                if(!$(e.target).closest('.sidebar').length && !$(e.target).is('.sidebar')) {
-                    $('.sidebar').removeClass('show');
-                } else {
-                    $(document).one('click', closeMenu);
-                }
-            });
-        });
-        try {
-            document.getElementsByClassName('view-icon')[0].click();
-        } catch (e) {
-            console.log('error while click on view btn', e);
-        }
-        $('.repost-btn').on('click', function (event) {
-            //$(document).ready(function(){
-            event.stopPropagation();
-            $('.repost-popup').css('left',event.target.x-150);      // <<< use pageX and pageY
-            $('.repost-popup').css('top',event.target.y+30);
-            $('.repost-popup').css('display','inline');     
-            $(".repost-popup").css("position", "absolute");  // <<< also make it absolute!
-            $(document).one('click', function closeMenu (e){
-                $('.repost-popup').css('display','none');  
-            });
-            $(window).one('resize', function closeMenu (e){
-                $('.repost-popup').css('display','none'); 
-                $(document).off('click'); 
-            });
-        });
-        //});
-
+    $(window).on('resize', function closeMenu(e) {
+      $('.title-pane').css('marginTop', $('.header-pane').outerHeight(true));
     });
+
+    $(window).resize();
+
+    $('.main-menu').on('click', function(event) {
+      event.stopPropagation();
+      $('.sidebar').addClass('show');
+      $(document).one('click', function closeMenu(e) {
+        if (!$(e.target).closest('.sidebar').length && !$(e.target).is('.sidebar')) {
+          $('.sidebar').removeClass('show');
+        } else {
+          $(document).one('click', closeMenu);
+        }
+      });
+    });
+    try {
+      document.getElementsByClassName('view-icon')[0].click();
+    } catch (e) {
+      console.log('error while click on view btn', e);
+    }
+
+    $('.repost-btn').on('click', onRepostBtnClick);
+
+  });
 })();
 
 App.modules.SVGSprites = {
-    init: function() {
-        var self = this;
-        $.get('media/svg/sprite.svg', function(data) {
-            $('<div style="width:0; height:0; overflow:hidden"></div>').prependTo(App.dom.$body).html(self.serializeXml(data.documentElement));
-        });
-    },
-    serializeXml: function(xmldom) {
-        if (typeof XMLSerializer != 'undefined') {
-            return (new XMLSerializer()).serializeToString(xmldom);
-        } else {
-            return $(xmldom).html();
-        }
+  init: function() {
+    var self = this;
+    $.get('media/svg/sprite.svg', function(data) {
+      $('<div style="width:0; height:0; overflow:hidden"></div>').prependTo(App.dom.$body).html(self.serializeXml(data.documentElement));
+    });
+  },
+  serializeXml: function(xmldom) {
+    if (typeof XMLSerializer != 'undefined') {
+      return (new XMLSerializer()).serializeToString(xmldom);
+    } else {
+      return $(xmldom).html();
     }
+  }
 };
