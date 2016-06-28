14d07eac2e1ae4e175f4c08d454681c47c90fa55
Tatyana C <tatyana_c@Unikey.local>
2016-06-28 18:47:38 +0400

    added app-page2 page

--- src/media/js/src/app.js
+++ src/media/js/src/app.js
@@ -19,8 +19,16 @@ function onLinkClicked(evt) {
 
 function onRepostBtnClick(event) {
   event.stopPropagation();
-  $('.repost-popup').css('left', event.target.x - 150);
-  $('.repost-popup').css('top', event.target.y + 30);
+  var popupW = 150, popupH = 30;
+  var leftpos = event.target.x - popupW;
+  if (leftpos <= 0) {
+    leftpos = 0;
+    $('.repost-popup').addClass('popup-left-arrow');
+  } else {
+    $('.repost-popup').removeClass('popup-left-arrow');
+  }
+  $('.repost-popup').css('left', leftpos);
+  $('.repost-popup').css('top', event.target.y + popupH);
   $('.repost-popup').css('display', 'inline');
   $(".repost-popup").css("position", "absolute");
   $(document).one('click', function closeMenu(e) {
@@ -43,6 +51,66 @@ function changeView(evt) {
     elem = elem.parentNode;
   }
   elem.className += ' selected';
+}
+
+function initCarousel(carousel) {
+    /* этот код помечает картинки, для удобства разработки */
+    var lis = carousel.getElementsByTagName('li');
+    for (var i = 0; i < lis.length; i++) {
+      lis[i].style.position = 'relative';
+      var span = document.createElement('span');
+      // обычно лучше использовать CSS-классы,
+      // но этот код - для удобства разработки, так что не будем трогать стили
+      span.style.cssText = 'position:absolute;left:0;top:0';
+      span.innerHTML = i + 1;
+      lis[i].appendChild(span);
+    }
+    $('.carousel li img').click(function(evt) {
+      if (evt.target.tagName != 'IMG')
+        return;
+      try {
+        document.getElementById('selected-image').src = evt.target.src;
+      } catch (e) {
+        console.log('error while selecting image in carousel:', e);
+      }
+      
+    })
+    $('.carousel .arrow').on('mouseover', function(evt) {
+      this.src = "media/img/icons/arrow-bright.png";
+    });
+    $('.carousel .arrow').on('mouseout', function(evt) {
+      this.src = "media/img/icons/arrow-light.png";
+    });
+
+    var count = 2;
+    var gallery = document.getElementsByClassName('gallery')[0];
+    var list = carousel.querySelector('ul');
+    var listElems = carousel.querySelectorAll('li');
+    var width = listElems[0].clientWidth + 20;
+    var position = 0;
+
+    carousel.querySelector('.prev').onclick = function() {
+      var wid = Math.min(width * count, document.getElementById('carousel').clientWidth/2);
+      position = Math.min(position + wid, 0)
+      list.style.marginLeft = position + 'px';
+      $('.carousel .next').css('opacity', '1');
+      if (position == 0) {
+        $('.carousel .prev').css('opacity', '0.3');
+      }
+    };
+
+    carousel.querySelector('.next').onclick = function() {
+      var wid = Math.min(width * count, document.getElementById('carousel').clientWidth/2);
+      //if it will be ent of list, and there are last images at screen. we don't need scroll white area
+      if (-gallery.clientWidth + position > -width * (listElems.length - count)) {
+        position = Math.max(position - wid, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'px';
+      } else {
+         $('.carousel .next').css('opacity', '0.3');
+      }
+      $('.carousel .prev').css('opacity', '1');
+    };
+
 }
 
 // Note: Use 'search & replace' to rename 'App' to current project name an delete this note
@@ -94,6 +162,12 @@ var App = new(function App() {
     } catch (e) {
       console.log('error while click on view btn', e);
     }
+
+
+
+    var carousel = document.getElementById('carousel');
+    if (carousel != undefined)
+      initCarousel(carousel);
 
     $('.repost-btn').on('click', onRepostBtnClick);
 
